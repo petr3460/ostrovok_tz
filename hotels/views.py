02d072bf8e63caaecd03a3e52ce5ed10d7cef6f9
Петр Галко <petr@Petrs-MacBook-Pro.local>
2018-08-05 18:54:23 +0300

    change view and html

--- hotels/views.py
+++ hotels/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from .models import City, Hotel
-from django.http import HttpResponse, JsonResponse
-import json
-import pdb
-# Create your views here.
+from django.http import JsonResponse
+
 
 
 def home(request):
@@ -13,8 +11,18 @@ def home(request):
         return render(request, 'home.html', context)
 
     if request.method == 'POST':
+        count = request.POST.get('count') or 0
+        show_button_again = 1
+        print('=====COUNT IS : ======= ', count)
 
-        hotels = Hotel.objects.filter(city=request.POST.get('cityselect')).values()
-        print(hotels.query)
-        #pdb.set_trace()
-        return JsonResponse({'hotels': list(hotels)})
+        if count:
+            count = int(count)
+            hotels = Hotel.objects.filter(city=request.POST.get('cityselect'))[count:count+3]
+
+        else:
+            hotels = Hotel.objects.filter(city=request.POST.get('cityselect'))[:3]
+
+        if not Hotel.objects.filter(city=request.POST.get('cityselect'))[count + 3:]:
+            show_button_again = 0
+
+        return JsonResponse({'hotels': list(hotels.values()), 'show_button': show_button_again})
